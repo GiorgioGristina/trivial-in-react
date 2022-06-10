27cd90f53cb247f76dfa10233d11990375e3495a
Giorgio Gristina <giorgio.gristina91@gmail.com>
2022-06-10 15:58:55 +0100

    create new React app; created button component to start the quiz and relate state boolean default false that when is click change the state, conditional rendering for the button start if is true disappear; api call to the trivial api

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
+import Button from './components/Button';
+import Question from './components/Question';
+
 import './App.css';
+import { useEffect, useState } from 'react';
+
 
 function App() {
+  const [started, setStarted] = useState(false)
+  const [questionAnswer, setQuestionAnswer] = useState([])
+  
+  useEffect(() => {
+    fetch("https://opentdb.com/api.php?amount=5&category=22&difficulty=medium&type=multiple")
+    .then(res => res.json())
+    .then(data => setQuestionAnswer(data.results))
+    
+  }, [])
+  // console.log(questionAnswer);
+
+
+
+  function startGame(){    
+    setStarted(oldStart => !oldStart)
+  }
+
+  const questionElements = questionAnswer.map((question) => {
+    <Question question={question.question} answers={[...question.correct_answer, ...question.incorrect_answers]} />
+  })
+
+  function shuffleAnswer(correct, incorrect){
+    // console.log(correct, incorrect);
+    let answer = [correct, ...incorrect];
+    console.log(answer);
+
+    // return answer
+  }
+
+  // console.log(shuffleAnswer(questionAnswer[0].correct_answer, questionAnswer[0].incorrect_answers))
+  console.log(questionAnswer.correct_answer)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className='container'>
+      { !started && <Button start={startGame}  className="start-button"/>}
+      {started && <Question />}
+    </section>
   );
 }
 
---
+++ src/components/Question.js
@@ -0,0 +1,19 @@
+
+function Question() {
+  
+
+  return (
+    <div> 
+      <h3> how many regio in italy?</h3>
+      <ul>
+        <li>15</li>
+        <li>21</li>
+        <li>24</li>
+        <li>18</li>
+
+      </ul>
+    </div>
+  );
+}
+
+export default Question;
