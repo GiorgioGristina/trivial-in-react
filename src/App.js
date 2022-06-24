4ac63404e25ef99febd794aac5b409f1ae9baafb
Giorgio Gristina <giorgio.gristina91@gmail.com>
2022-06-24 15:32:58 +0100

    modified apical to run only if the started state is true and deleted the mapping of the question because I decided to improve the structure adding more states

--- src/App.js
+++ src/App.js
@@ -7,36 +7,83 @@ import { useEffect, useState } from 'react';
 
 function App() {
   const [started, setStarted] = useState(false)
-  const [questionAnswer, setQuestionAnswer] = useState([])
-
-  
+  const [data, setData] = useState([])
+  const [ question, setQuestions] = useState([])
+  // const [isContainerActive, setIsContainerActive] = useState(false);
   // const answerShuf = []
   useEffect(() => {
-    fetch("https://opentdb.com/api.php?amount=5&category=22&difficulty=medium&type=multiple")
-    .then(res => res.json())
-    .then(data => setQuestionAnswer(data.results))
-    
-  }, [])
+    if (started) {
+      fetch("https://opentdb.com/api.php?amount=5&category=22&difficulty=medium&type=multiple")
+      .then(res => res.json())
+      .then(data => setData(data.results))     
+    }
+  }, [started])
+
+  useEffect(() => {
+    function generateAnswer(data){
+      const correct = data.correct_answer
+      let incorrect = data.incorrect_answers
+      const randomNum = Math.floor(Math.random() * 4);
+      const newArray = [...incorrect]
+      newArray.splice(randomNum, 0, correct)
+      return newArray;   
+    }
+    function generateAnswerObj(data){
+      const answerOption = generateAnswer(data)
+      answerOption.map((answer, i) => {
+        return {
+          id: i,
+          answer: answer,
+          selected: false
+        }
+      })
+      return data
+    }
+    const generateQuestionOBject = (object) => {
+      return {
+        question: object.question,
+        correctAnswer: object.correct_answer,
+        answers: generateAnswerObj(object)
+      }
+    }
+    setQuestions( data.map((item) => generateQuestionOBject(item)))
+  }, [data])
+  
+  console.log(question);
+  // console.log(data);
+  // data.map((obj => {
+  //   generateAnswer(obj)
+
+  // }))
+
+
+
+  // console.log(question);
 
   function handleClick(e){
-    console.log(e.target.innerText)
+    // // console.log()
+    // // e.target.className("active")
+    // console.log(e.currentTarget);
+    // setIsContainerActive(prev => !prev)
   }
  
   function startGame(){    
     setStarted(oldStart => !oldStart)
   }
 
-  const questionElements = questionAnswer.map((question) => {
-    return <Question click={handleClick}
-                     question={question.question} 
-                     answerCorrect={question.correct_answer} 
-                     answersIncorrect={question.incorrect_answers} />
-  })
+  // const questionElements = questionAnswer.map((question, index) => {
+  //   return <Question click={handleClick}
+  //                    key={index}
+  //                    activeContainer={isContainerActive}
+  //                    question={question.question} 
+  //                    answers={question.option} 
+  //                     />
+  // })
 
   return (
     <section className='container'>
       { !started && <Button start={startGame}  className="start-button"/>}
-      {started && questionElements}
+      {/* {started && questionElements} */}
     </section>
   );
 }
--- src/components/Question.js
+++ src/components/Question.js
@@ -1,36 +1,15 @@
 
-function Question(props) {  
-  const answers = [props.answerCorrect, ...props.answersIncorrect];
+function Question({question, answers, click}) {  
+ 
   
-
-  const shuffleArray = array => {
-    // const ans = []
-      for (let i = array.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      const temp = array[i];
-      array[i] = array[j];
-      array[j] = temp;
-      return array
-    }
-    // return ans    
-  }
-  
-  
-  const s = shuffleArray(answers)
-  // console.log(props.answerCorrect, s)
-  
-  // const s = shuffleArray(props.answerCorrect)
-  // const answers =  shuffleAnswer(props.answersIncorrect, props.answerCorrect)
-  // console.log(answers)
   return (
     <div> 
-      <h3>{props.question}</h3>
+      <h3>{question}</h3>
       <ul>
-        <li onClick={props.click}>{s[0]}</li>
-        <li onClick={props.click}>{s[1]}</li>
-        <li onClick={props.click}>{s[2]}</li>
-        <li onClick={props.click}>{s[3]}</li>
-
+        <li  onClick={click}>{answers[0].answer}</li>          
+        <li  onClick={click}>{answers[1].answer}</li>
+        <li  onClick={click}>{answers[2].answer}</li>
+        <li  onClick={click}>{answers[3].answer}</li>
       </ul>
     </div>
   );
